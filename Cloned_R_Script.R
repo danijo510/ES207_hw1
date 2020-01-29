be157311d267d1c3814cc315ad1db2fe5d4607e2
Danielle Jones <djones47@ucmerced.edu>
2020-01-29 12:29:23 -0800

    From EasyD's IntroToDataScience Repo

---
+++ Cloned_R_Script.R
@@ -0,0 +1,19 @@
+##Script found from EasyD in the IntroToDataScience repo
+##FOr local testing, a CSV file was loaded onto my computer to make sure it all worked
+
+
+extractTitle <- function(name) {
+  name <- as.character(name)
+  
+  if (length(grep("Miss.", name)) > 0) {
+    return ("Miss.")
+  } else if (length(grep("Master.", name)) > 0) {
+    return ("Master.")
+  } else if (length(grep("Mrs.", name)) > 0) {
+    return ("Mrs.")
+  } else if (length(grep("Mr.", name)) > 0) {
+    return ("Mr.")
+  } else {
+    return ("Other")
+  }
+}
